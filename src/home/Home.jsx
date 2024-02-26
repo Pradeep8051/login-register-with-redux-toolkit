83d5ab532864ed31cd7004abe0c3b272a57ab141
pradeep8051 <pradeepchauhan8051@gmail.com>
2024-02-26 16:59:21 +0530

    adding the login regiter with redux-toolkit

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
     return (
         <div>
             <h1>Hi {auth?.firstName}!</h1>
-            <p>You're logged in with React 18 + Redux & JWT!!</p>
+            <p>You're logged in Baseline it devlepment!!</p>
             <p><Link to="/users">Manage Users</Link></p>
         </div>
     );
